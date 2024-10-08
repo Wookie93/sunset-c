a62a07a4846bf24a3b357f1d76d72f06570fb38b
Lukasz T <lukasz.trzos1@gmail.com>
2024-10-08 23:39:25 +0200

    added revalidation

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,16 +1,43 @@
-// import { NextRequest, NextResponse } from 'next/server';
-// import { revalidatePath } from 'next/cache';
+import { NextRequest, NextResponse } from 'next/server';
+import { revalidatePath } from 'next/cache';
 
-// export async function POST(request: NextRequest) {
-//   const secret = request.nextUrl.searchParams.get('secret');
+const routesToRevalidate: string[] = [
+    '/',
+    '/about', '/o-domku',
+    '/pricing', '/cennik',
+    '/contact', '/kontakt',
+    '/neighborhood', '/okolica',
+    '/gallery', '/galeria',
+    '/gallery/:path*', '/galeria/:path*'
+  ];
 
-//   if (secret !== process.env.REVALIDATION_TOKEN) {
-//     return NextResponse.json({ message: 'Invalid token' }, { status: 401 });
-//   }
-
-//   const path = request.nextUrl.searchParams.get('path') || '/about-us';
-
-//   revalidatePath(path);
-
-//   return NextResponse.json({ revalidated: true, now: Date.now() });
-// }
+  export async function POST(request: NextRequest): Promise<NextResponse> {
+    const requestHeaders: Headers = new Headers(request.headers);
+    const secret: string | null = requestHeaders.get("x-vercel-reval-key");
+  
+    if (secret !== process.env.CONTENTFUL_REVALIDATE_SECRET) {
+      return NextResponse.json(
+        { message: "Invalid secret" }, 
+        { status: 401 }
+      );
+    }
+  
+    try {
+      routesToRevalidate.forEach((route: string) => {
+        revalidatePath(route);
+      });
+  
+      return NextResponse.json({
+        revalidated: true,
+        now: Date.now(),
+        message: "Revalidated all routes"
+      });
+    } catch (error) {
+      const errorMessage: string = error instanceof Error ? error.message : 'Unknown error occurred';
+      
+      return NextResponse.json({
+        message: "Error revalidating",
+        error: errorMessage
+      }, { status: 500 });
+    }
+  }
