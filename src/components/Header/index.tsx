e48f83f8fa3a3582bbf05bf329ead63366bd0a16
k_czech <kamczeh@gmail.com>
2024-10-09 10:15:01 +0200

    added static logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,16 @@
 'use client'
 
 import NextLink from "next/link";
-import { Logo } from "@/assets/logo/Logo";
-import { clsx } from "clsx";
-import { MobileMenuHandler } from "@/components/MobileMenuHandler";
-import { useNavigationContext } from "@/providers/navigation-provider";
-import { IconMenu2, IconX } from "@tabler/icons-react";
-import { Navigation } from "../Navigation";
-import { usePathname } from "next/navigation";
-import { ContenfulHeaderModuleItem, ContenfulNavigationLink } from "@/types/contefulTypes";
-import { ButtonLink } from "../ButtonLink";
+import {clsx} from "clsx";
+import {MobileMenuHandler} from "@/components/MobileMenuHandler";
+import {useNavigationContext} from "@/providers/navigation-provider";
+import {IconMenu2, IconX} from "@tabler/icons-react";
+import {Navigation} from "../Navigation";
+import {usePathname} from "next/navigation";
+import {ContenfulHeaderModuleItem, ContenfulNavigationLink} from "@/types/contefulTypes";
+import {ButtonLink} from "../ButtonLink";
+import {LogoDark} from "@/assets/logo/LogoDark";
+import {LogoColor} from "@/assets/logo/LogoColor";
 
 interface HeaderProps{
 	headerData: {
@@ -28,7 +29,7 @@ interface HeaderProps{
 
 export const Header = ({headerData, navigationLinks}:HeaderProps) => {
 	const pathname = usePathname();
-	
+
 	const {modulesCollection} = headerData
 	console.log(modulesCollection)
 
@@ -37,7 +38,7 @@ export const Header = ({headerData, navigationLinks}:HeaderProps) => {
 		<header className="absolute left-0 right-0 top-0 z-[101] w-full py-6">
 			<div className="container mx-auto flex flex-row items-center justify-between">
 				<NextLink href="/">
-					{isOpen || pathname !== "/" ? <Logo url={headerData.logoDark.url} /> : <Logo url={headerData.logo.url} />}
+					{isOpen || pathname !== "/" ? <LogoDark /> : <LogoColor />}
 				</NextLink>
 				<Navigation navigationLinks={navigationLinks} className={clsx(
 						"hidden laptop:flex",
