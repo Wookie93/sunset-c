cf65c9f0b44f1bf9fa0ed986ccd76ce17082a297
k_czech <kamczeh@gmail.com>
2024-10-12 11:10:32 +0200

    changed url

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -1,6 +1,6 @@
 import { GraphQLClient } from 'graphql-request';
 
-const client = new GraphQLClient(process.env.CONTENTFUL_API_URL!, {
+const client = new GraphQLClient(process.env.CONTENTFUL_GRAPHQL_ENDPOINT!, {
   headers: {
     Authorization: `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}`,
   },
