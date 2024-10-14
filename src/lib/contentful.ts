37431dd92921629f7dda659b49d3720503d95a6c
k_czech <kamczeh@gmail.com>
2024-10-14 09:12:28 +0200

    removed no-store property in graphql-request cache

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -4,7 +4,6 @@ const client = new GraphQLClient(process.env.CONTENTFUL_GRAPHQL_ENDPOINT!, {
   headers: {
     Authorization: `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}`,
   },
-  cache: 'no-store',
 });
 
 export default client;
