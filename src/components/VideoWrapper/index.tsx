03fe0125e7dc6f87c1d695baecdb0eac832d8397
k_czech <kamczeh@gmail.com>
2024-10-14 10:46:10 +0200

    changed video dimensions

--- src/components/VideoWrapper/index.tsx
+++ src/components/VideoWrapper/index.tsx
@@ -2,7 +2,7 @@ export const VideoWrapper = ({videoUrl}: {videoUrl: string}) => {
     return (
     <div className="col-span-1 max-tabletLg:order-2 max-tabletLg:pt-[3.125rem] tabletLg:col-span-6">
         <div className="relative flex h-lvh max-h-[24.375rem] items-center justify-center laptop:max-h-[32.75rem]">
-            <video controls >
+            <video controls className="h-full w-full object-cover">
                 <source src={videoUrl} type="video/mp4" />
       Your browser does not support the video tag.
     </video>
